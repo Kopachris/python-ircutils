a6c426f042f54a91b13dba1385b03b08a6f1293f
Kopachris <kopachris@gmail.com>
2017-08-26 16:39:34 -0600

    new license, BSD 3-clause

---
+++ ircutils/__init__.py
@@ -0,0 +1,7 @@
+"""IRCUtils is a Python framework for communicating with the  IRC protocol.
+It's designed for ease of use for anything you'd need. It also has fast
+and simple support for writing IRC bots.
+"""
+
+
+__author__ = "Christopher Koch"
